e519b1ab53db2ddac17affcb74fb1ee4aae5e970
Frozen Helium <fren.ankit@gmail.com>
2016-11-03 21:33:36 +0545

    Fix task placement

--- dashboard.js
+++ dashboard.js
@@ -13,7 +13,7 @@ refreshTasks = function() {
 
     var taskTemplate = $(".task-template");
     var timeTemplate = $(".time-template");
-    var taskContainer = $("#task-list")
+    var taskContainer = $("#task-list .row")
 
     taskContainer.empty();
 
@@ -21,7 +21,7 @@ refreshTasks = function() {
         var task = taskTemplate.clone();
         var currentTask = tasks[i];
         task.removeClass("task-template");
-        task.addClass("task");
+        //task.addClass("task");
 
         task.find("h3").text(currentTask.title);
         task.find("p").text(projects[currentTask.project]);
